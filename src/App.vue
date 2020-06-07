074f3cec43af83ca95e6d344ea447983f4f52d2a
EdenSheng <singlesaul@163.com>
2020-06-07 10:34:49 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <div id="app">
+  <div id="app" :class="'theme'+'-'+bg">
     <img v-y width="25%" src="./assets/logo.png">
+    <ChangeButton/>
   </div>
 </template>
 
 <script>
 import log from './mixins/log'
+import ChangeButton from './components/ChangeButton'
 export default {
   name: "App",
+  data(){
+    return{
+      bg: 'pink'
+    }
+  },
+  components:{ChangeButton},
   mixins: [log],
   directives:{
     y:{
@@ -16,6 +24,20 @@ export default {
           console.log('y')
         })
       }
+    }
+  },
+  methods:{
+    changeTheme(){
+      if(this.bg==='pink'){
+        this.bg ='orange'
+      }else{
+        this.bg ='pink'
+      }
+    }
+  },
+  provide(){
+    return {
+      changeTheme:this.changeTheme
     }
   }
 };
@@ -29,5 +51,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app.theme-pink{
+  background: pink;
+}
+#app.theme-orange{
+  background: orange;
 }
 </style>
---
+++ src/components/ChangeButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="x">换肤功能</button>
+  </div>
+</template>
+
+<script>
+export default {
+    inject:['changeTheme'],
+    methods:{
+      x(){
+        this.changeTheme()
+      }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
